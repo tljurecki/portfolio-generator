b74ef23d6b31b609b8310a59346549e95ab3818b
Tanya Jurecki <tljurecki@gmail.com>
2022-04-14 14:28:42 -0400

    added function to generate html template

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require(`fs`);
 
-const printProfileData = profileDataArr => {
-    for(let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log("=============");
+const generatePage = require('./src/page-template.js');
 
-profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw err;
+
+    console.log('Portfolio complete!  Check out index.html to see the output!');
+});
 
 
-printProfileData(profileDataArgs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const printProfileData = profileDataArr => {
+    //for(let i = 0; i < profileDataArr.length; i++) {
+        //console.log(profileDataArr[i]);
+    //}
+    //console.log("=============");
+
+//profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
+
+
+
+
+
+//printProfileData(profileDataArgs);
