1410be7b64bae18f2c406958620497279505e6f7
Tanya Jurecki <tljurecki@gmail.com>
2022-04-14 14:47:06 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const fs = require(`fs`);
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw err;
+
+    console.log('Portfolio complete!  Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const printProfileData = profileDataArr => {
+    //for(let i = 0; i < profileDataArr.length; i++) {
+        //console.log(profileDataArr[i]);
+    //}
+    //console.log("=============");
+
+//profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
+
+
+
+
+
+//printProfileData(profileDataArgs);
