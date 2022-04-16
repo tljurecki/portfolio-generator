b2998d59fb8e6910708d65c0cc95aba80462064e
Tanya Jurecki <tljurecki@gmail.com>
2022-04-16 13:59:45 -0400

    updated html template to handle more information

--- app.js
+++ app.js
@@ -130,7 +130,13 @@ const promptProject = portfolioData => {
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+  
+    fs.writeFile('./index.html', pageHTML, err => {
+    if(err) throw new Error(err);
+
+    console.log('Page created!  Check out index.html in this directory to see it!');
+});
 });
 
 
